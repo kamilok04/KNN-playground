fd5ed46b8f1e937217c3a3de413f31f3efc7f5b2
Paskal Maruska <pm310865@student.polsl.pl>
2025-05-11 15:55:10 +0200

    Udpated data analysis

--- analyzeData.py
+++ analyzeData.py
@@ -1,32 +1,29 @@
-import pandas
-import matplotlib.pyplot as plt
+﻿import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 class analyzeData:
-    def __init__(self,path):
-        self.df = pandas.read_csv(path)
+    def __init__(self,path,numerical_columns, category_columns):
+        self.df = pd.read_csv(path)
+        self.num_cols = numerical_columns
+        self.cat_cols = category_columns
         self.df = self.df.dropna()
-
-    
-    def plot(self):
-        """
-        Plot the data using seaborn
-        """
-        sns.pairplot(self.df)
-        plt.show()
 
     def describe(self):
         """
         Print the description of the data
         """
+        print(self.df.info())
         print(self.df.describe())
+        print(self.df.head())
 
     def correlation(self):
         """
-        Print the correlation of the data
+        Print the correlation matrix of the data
         """
-        print(self.df.corr())
-        sns.heatmap(self.df.corr(), annot=True)
+        corr = self.df.corr()
+        sns.heatmap(corr, annot=True, cmap='coolwarm')
+        plt.title('Correlation matrix')
         plt.show()
 
     def preprocess_data(self):
@@ -34,23 +31,66 @@ class analyzeData:
         Define dictionaries containing all non-number features so that they can be mapped to numbers.
         """
         for column in self.df:
-            self.df = self.df.apply(lambda x: pandas.factorize(x)[0])
+            self.df = self.df.apply(lambda x: pd.factorize(x)[0])
 
-    def shuffle(self, data = None, ratio = 70):
-        if data is None:
-            data = self.df
-        border = len(data) * ratio // 100
-        shuffled = data.sample(frac=1)
-        control = shuffled.iloc[:border]
-        test = shuffled.iloc[border:]
-        return control, test
+    def numericalDistribution(self):
+        """
+        Print the distribution of the numerical columns
+        """
+        for col in self.num_cols:
+            sns.histplot(self.df[col], kde=True)
+            plt.title(f'Distribution: {col}')
+            plt.show()
 
-    
-           
-dataAnalytics = analyzeData('student-mat.csv')
-dataAnalytics.preprocess_data()
-#dataAnalytics.plot()
-dataAnalytics.describe()
-dataAnalytics.correlation()
+    def uniqueValues(self):
+        """
+        Print the unique values of the categorical columns
+        """
+        for col in self.cat_cols:
+            print(f'{col}: {self.df[col].unique()}')
+
+    def categoricalDistribution(self):
+        """
+        Print the distribution of the categorical columns compared to heart disease
+        """
+        for col in self.cat_cols:
+            sns.countplot(data=self.df, x=col, hue='HeartDisease')
+            plt.title(f'{col} distribution compared to HeartDisease')
+            plt.show()
+
+    def pairPlot(self, columns):
+        """
+        Print the pair plot of chosen data
+        """
+        sns.pairplot(self.df, hue='HeartDisease', vars=columns)
+        plt.show()
+
+    def boxPlot(self, x_column, y_column):
+        """
+        Print the box plot of the chosen data
+        """
+        sns.boxplot(x=x_column, y=y_column, data=self.df)
+        plt.title(f'{x_column} vs {y_column}')
+        plt.show()
+
+    def driver(self):
+        self.describe()
+        self.uniqueValues()
+        self.categoricalDistribution()
+        self.numericalDistribution()
+        self.pairPlot(['Age', 'RestingBP', 'Cholesterol', 'MaxHR', 'Oldpeak'])
+        self.boxPlot('HeartDisease', 'Cholesterol')
+        self.preprocess_data()
+        self.correlation()
+
+dataAnalytics = analyzeData('heart.csv',
+['Age', 'RestingBP', 'Cholesterol', 'MaxHR', 'Oldpeak','HeartDisease'],
+['Sex', 'ChestPainType', 'RestingECG', 'ExerciseAngina', 'ST_Slope','FastingBS'])
+
+
+dataAnalytics.driver()
+# dataAnalytics.preprocess_data()
+# dataAnalytics.correlation()
+
 
 
